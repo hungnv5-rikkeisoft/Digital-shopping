f0effaae1323178e54ecb1da48ebdb90b18c0837
Hung Nguyen <hungnv5@rikkeisoft.com>
2025-06-25 09:50:36 +0700

    init project base

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './core/auth/pages/login/login.component';
+import { ProductListComponent } from './features/products/pages/product-list/product-list.component';
+import { FavoritesListComponent } from './features/favorites/pages/favorites-list/favorites-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'products',
+    component: ProductListComponent,
+    // canActivate: [authGuard] // Will be added later
+  },
+  {
+    path: 'favorites',
+    component: FavoritesListComponent,
+    // canActivate: [authGuard] // Will be added later
+  },
+  {
+    path: '',
+    redirectTo: 'products',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'products',
+    pathMatch: 'full',
+  },
+];
